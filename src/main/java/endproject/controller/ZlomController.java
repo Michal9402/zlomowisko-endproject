8a2f53c6305ea5b154c9cd9b3e1ace3b12c966ca
michal czarkowski <m.czarkowski94@gmail.com>
2020-07-02 22:23:10 +0200

    finished admin page

--- src/main/java/endproject/controller/ZlomController.java
+++ src/main/java/endproject/controller/ZlomController.java
@@ -5,12 +5,13 @@ import endproject.service.ZlomService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 
 @Controller
-public class ZlomController  {
+public class ZlomController {
 
     private final ZlomService serv;
 
@@ -24,15 +25,34 @@ public class ZlomController  {
         model.addAttribute("zlomList", zlomList);
         return "index";
     }
+
     @RequestMapping("/new")
-    public String newZlomProduct(Model model) {
+    public String showNewZlomProductForm(Model model) {
         Zlom zlom = new Zlom();
         model.addAttribute("zlom", zlom);
         return "new_product";
     }
+
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String saveProduct(@ModelAttribute("zlom") Zlom zlom){
+    public String saveProduct(@ModelAttribute("zlom") Zlom zlom) {
         serv.save(zlom);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditZlomProductForm(@PathVariable(name = "id") Long id) {
+        ModelAndView mvn = new ModelAndView("edit_product");
+
+        Zlom zlom = serv.getById(id);
+        mvn.addObject("zlom", zlom);
+
+        return mvn;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") Long id) {
+        serv.deleteById(id);
+
         return "redirect:/";
     }
 }
